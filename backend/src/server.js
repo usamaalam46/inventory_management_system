2a630c7a05ffc6cb6cb2fd3c5ada58d84579a98d
usamaalam46 <58341980+usamaalam46@users.noreply.github.com>
2026-05-10 15:23:47 +0500

    update the server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,12 +5,9 @@ const app = require('./app');
 const PORT =
   process.env.PORT || 5000;
 
-const APP_URL =
-  process.env.APP_URL || "localhost";
-
 app.listen(PORT, () => {
 
   console.log(
-    `✅ Server running on ${APP_URL}:${PORT}`
+    `✅ Server running on port ${PORT}`
   );
 });
